4d8f83e6dae0df0a2df04a988b4eb6d77279e5d0
Jason E. Aten <j.e.aten@gmail.com>
2015-12-20 11:18:34 -0800

    atg. confirm zipfile content also readback correctly.

--- libzipfs_test.go
+++ libzipfs_test.go
@@ -3,6 +3,8 @@ package libzipfs
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 	"testing"
 
 	cv "github.com/glycerine/goconvey/convey"
@@ -11,7 +13,8 @@ import (
 func Test001WeCanMountInTheTmpDir(t *testing.T) {
 
 	cv.Convey("we should be able to mount a zipfile image in the tmp dir", t, func() {
-		dir := "/tmp" // "" => use system tmp dir
+		dir := "/tmp" // => /tmp easier to debug/shorter to type.
+		// dir := "" // => use system tmp dir
 		mountPoint, err := ioutil.TempDir(dir, "libzipfs")
 		VPrintf("\n\n mountPoint = '%s'\n", mountPoint)
 		cv.So(err, cv.ShouldEqual, nil)
@@ -26,6 +29,21 @@ func Test001WeCanMountInTheTmpDir(t *testing.T) {
 		}
 
 		VPrintf("\n\n z.Start() succeeded, with mountPoint = '%s'\n", mountPoint)
+		expectedFile := path.Join(mountPoint, "dirA", "dirB", "hello")
+		expectedFileContent := []byte("salutations\n")
+
+		fmt.Printf("\n   we should be able to read back a file from the mounted filesystem without errors.\n")
+		ef, err := os.Open(expectedFile)
+		cv.So(err, cv.ShouldBeNil)
+		cv.So(ef, cv.ShouldNotBeNil)
+		ef.Close()
+		ef = nil
+		by, err := ioutil.ReadFile(expectedFile)
+		cv.So(err, cv.ShouldBeNil)
+		cv.So(len(expectedFileContent), cv.ShouldEqual, len(by))
+		diff, err := compareByteSlices(expectedFileContent, by, min(len(expectedFileContent), len(by)))
+		cv.So(err, cv.ShouldBeNil)
+		cv.So(diff, cv.ShouldEqual, -1)
 
 		err = z.Stop()
 		if err != nil {
@@ -35,4 +53,17 @@ func Test001WeCanMountInTheTmpDir(t *testing.T) {
 		VPrintf("\n\n z.Stop() succeeded, with mountPoint = '%s'\n", mountPoint)
 	})
 
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
